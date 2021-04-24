61915d5ff5fdaf72c3d1d6bef735e61045224e11
zyakin <zyakin33@yandex.ru>
2021-04-24 15:15:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// const category = 'toys';
+
+// Старый способ
+// console.log('https:///someurl.com/' + category + '/' +'5');
+
+// Новый способ интерполяции
+// console.log(`https:///someurl.com/${category}/5`);
